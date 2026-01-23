325a44832ec2072dab57295fa5872de2d0818552
Ville Nupponen <ville.nupponen@hewo.fi>
2026-01-23 22:28:34 +0200

    Add pulse animation for hero image

--- apps/landing-page/src/app/page.tsx
+++ apps/landing-page/src/app/page.tsx
@@ -7,12 +7,34 @@ import styles from './styles.module.scss';
 const Page = () => (
     <div className={styles.page}>
         <div className={styles.hero}>
-            <Image
-                src='/images/background.png'
-                alt='Piirroskuva Minecraft-pelaajista yhteydessä toisiinsa Internetin välityksellä'
-                width={1024}
-                height={1024}
-            />
+            <div className={styles.image}>
+                <Image
+                    src='/images/background.png'
+                    alt='Piirroskuva Minecraft-pelaajista yhteydessä toisiinsa Internetin välityksellä'
+                    width={1024}
+                    height={1024}
+                />
+
+                <div className={styles.dot1_a} />
+                <div className={styles.dot1_b} />
+                <div className={styles.dot1_c} />
+                <div className={styles.dot1_d} />
+
+                <div className={styles.dot2_a} />
+                <div className={styles.dot2_b} />
+                <div className={styles.dot2_c} />
+                <div className={styles.dot2_d} />
+
+                <div className={styles.dot3_a} />
+                <div className={styles.dot3_b} />
+                <div className={styles.dot3_c} />
+                <div className={styles.dot3_d} />
+
+                <div className={styles.dot4_a} />
+                <div className={styles.dot4_b} />
+                <div className={styles.dot4_c} />
+                <div className={styles.dot4_d} />
+            </div>
             <div className={styles.content}>
                 <h1>Lag is over. Your blocky empire awaits</h1>
                 <p>
